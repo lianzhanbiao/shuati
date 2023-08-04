de1aeda28ac53186c8d5b8a3e73c1e20cdf0d19e
lianzhanbiao <lzbcs@outlook.com>
2023-08-04 17:21:51 +0800

    直接模拟

--- src/huawei/P1026.java
+++ src/huawei/P1026.java
@@ -1,27 +1,6 @@
 package huawei;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
-
-class pp {
-    int x, y;
-
-    pp(int a, int b) {
-        this.x = a;
-        this.y = b;
-    }
-}
-
-class cmp implements Comparator<pp> {
-    public int compare(pp a, pp b) {
-        if (a.x != b.x)
-            return a.x - b.x;
-        return a.y - b.y;
-    }
-}
+import java.util.*;
 
 class P1026 {
     public static void main(String[] args) {
@@ -38,25 +17,23 @@ class P1026 {
             cnt[t]++;
             nowcnt++;
             if (nowcnt == num) { // 如果记录的数量到达应该计算的增量时统计答案
-                List<pp> tmparr = new ArrayList<>();
+                List<int[]> tmparr = new ArrayList<>();
                 // 依次遍历数字，小写字母，大写字母，存入临时数组中
                 for (int i = 'a'; i <= 'z'; i++) {
-                    tmparr.add(new pp(cnt[i], i));
+                    tmparr.add(new int[]{cnt[i], i});
                 }
                 for (int i = 'A'; i <= 'Z'; i++) {
-                    tmparr.add(new pp(cnt[i], i));
+                    tmparr.add(new int[]{cnt[i], i});
                 }
                 for (int i = '0'; i <= '9'; i++) {
-                    tmparr.add(new pp(cnt[i], i));
+                    tmparr.add(new int[]{cnt[i], i});
                 }
                 // 重载排序按第一维数量排，第一维相等时按第二维字母ascii码排
-                Collections.sort(tmparr, new cmp());
+                Collections.sort(tmparr, (p1, p2) -> p1[0]==p2[0]?p2[1]-p1[1]:p2[0]-p1[0]);
                 // 统计前 m 个输出
                 int tmp = M;
-                while (tmparr.size() > 0 && tmp != 0) {
-                    tmp--;
-                    System.out.print((char) tmparr.get(tmparr.size() - 1).y);
-                    tmparr.remove(tmparr.size() - 1);
+                for (int k=0; k<tmp; k++) {
+                    System.out.print((char) tmparr.get(k)[1]);
                 }
                 nowcnt = 0;
             }
