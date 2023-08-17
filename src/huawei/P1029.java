cebc3058731b0ade3942ebaad1925ce439b235ce
lzb <lzb@huipu2>
2023-08-17 20:47:10 +0800

    基础要打扎实

---
+++ src/huawei/P1029.java
@@ -0,0 +1,12 @@
+package huawei;
+
+import java.util.*;
+
+public class P1029 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+    }
+}
